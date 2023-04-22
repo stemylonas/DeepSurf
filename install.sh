b55c48203053fcd2cd738df1f00a247b4eadeec6
mainguyenanhvu <26211591+mainguyenanhvu@users.noreply.github.com>
2023-04-22 10:40:54 +0700

    Remove adundant dependencies and fix order install

--- install.sh
+++ install.sh
@@ -7,11 +7,8 @@ sudo apt install -y g++
 sudo apt install -y swig
 conda install pytorch==1.2.0 torchvision==0.4.0 cudatoolkit=10.0 -c pytorch
 conda install -c conda-forge openbabel
-pip install numpy==1.13.3
 pip install tensorflow-gpu==1.13.1
-pip install scipy==1.2.2
 pip install scikit-learn==0.20.3
-pip install protobuf==3.6.1
 
 git clone https://github.com/stemylonas/DeepSurf
 cd DeepSurf
@@ -21,7 +18,7 @@ chmod a+x compile.sh
 ./compile.sh
 cd ..
 
-pip install gdown
+pip install gdown  --ignore-installed certifi
 gdown 1nIBoD3_5nuMqgRGx4G1OHZwLsiUjb7JG
 p7zip -d models.7z
 
@@ -29,5 +26,6 @@ wget www.cgl.ucsf.edu/Overview/ftp/dms.zip
 unzip dms.zip
 rm dms.zip
 cd dms
+chmod 777 -R .
 sudo make install
 cd ..
